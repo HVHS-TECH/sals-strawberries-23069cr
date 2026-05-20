032eee56f906a11162c5679ac665c20fc689b9cd
23069cr <23069cr@hvhs.school.nz>
2026-05-20 22:26:19 +0000

    I just started

---
+++ fb_setup.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqVydCqpAvM5uc9fT2_IEQ3qExtqpAyFQ",
+  authDomain: "sal-sstrawberries.firebaseapp.com",
+  databaseURL: "https://sal-sstrawberries-default-rtdb.firebaseio.com",
+  projectId: "sal-sstrawberries",
+  storageBucket: "sal-sstrawberries.firebasestorage.app",
+  messagingSenderId: "397104764316",
+  appId: "1:397104764316:web:8286b3e80ec23a59d5a459",
+  measurementId: "G-BHL7MWG7Q2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
