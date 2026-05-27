a6865d982838c670ea8c687f809b3ccafe609d63
23069cr <23069cr@hvhs.school.nz>
2026-05-27 22:55:02 +0000

    trying to get rid of dima's cat's situation from my firebase database

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
 console.log("Running Sal's Strawberries")
 
-function writeForm(){
+function writeForm() {
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
 }
+
+foodOrder = {
+    game1: {
+        users: {
+            Josh: "help",
+            Coby: "sausages",
+            Pasha: "steak",
+            Lukas:"chicken",
+        }
+    }
+}
+firebase.database().ref('/').set(foodOrder)
